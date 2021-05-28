a9fc2bb6cb0b0dbf03aea790af003694b2fdc741
Keisuke Ito <k.app.dev.k@gmail.com>
2021-05-29 00:09:39 +0900

    軽微な修正

--- todo/models.py
+++ todo/models.py
@@ -6,7 +6,6 @@ from norilog.models import CustomUser
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.CASCADE)
-    # body = models.CharField(max_length=200, verbose_name='タスク')
     body = models.TextField(verbose_name='タスク')
     created_at = models.DateTimeField(default=timezone.now ,verbose_name='作成日')
 
